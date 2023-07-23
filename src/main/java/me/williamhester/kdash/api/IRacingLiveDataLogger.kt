e29419e36f6032e6c87833a0b614607a999854c5
William Hester <williamehester@gmail.com>
2023-07-23 15:53:16 -0700

    Fix IRacingLiveDataLogger to properly format the file
    This is very ugly code, but I'll reformat it on my laptop.

--- src/main/java/me/williamhester/kdash/api/IRacingLiveDataLogger.kt
+++ src/main/java/me/williamhester/kdash/api/IRacingLiveDataLogger.kt
@@ -1,10 +1,15 @@
 package me.williamhester.kdash.api
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import java.util.Scanner
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
+import kotlin.io.path.absolute
 
 /**
  * A live data reader that logs to a file.
@@ -27,46 +32,74 @@ class IRacingLiveDataLogger(
   fun collectAndLog() {
     val buffers = mutableListOf<ByteArray>()
     var numRead = 0
-    while (isRunning.get() && liveReader.hasNext()) {
-      if (numRead % 600 == 0) println("Read $numRead buffers")
-      buffers.add(liveReader.next().byteBuffer.array())
-      numRead++
-    }
+    try {
+      while (isRunning.get() && liveReader.hasNext()) {
+        if (numRead % 600 == 0) println("Read $numRead buffers")
+        val array = ByteArray(liveReader.fileHeader.bufLen)
+        val nextBuffer = liveReader.next()
+        nextBuffer.byteBuffer.get(array)
+        buffers.add(array)
+        numRead++
 
-    val headerBuffer = liveReader.fileHeader.buffer
-    headerBuffer.putInt(140, buffers.size) // Write the number of records to the "disk subheader"
+        println(nextBuffer.getFloat("Speed"))
+      }
 
-    val file = Files.createFile(outputPath)
-    Files.newOutputStream(file).use { outputStream ->
-      outputStream.write(headerBuffer.array())
+      val readOnlyHeaderBuffer = liveReader.fileHeader.buffer
+      val headerBuffer = ByteBuffer.allocate(144).order(ByteOrder.LITTLE_ENDIAN)
+      readOnlyHeaderBuffer.get(headerBuffer.array())
+      headerBuffer.putInt(20, 144) // Override the sessionInfoOffset to look like a logged data header
+      headerBuffer.putInt(28, 144 + liveReader.fileHeader.sessionInfoLen)
+      headerBuffer.putInt(140, buffers.size) // Write the number of records to the "disk subheader"
 
-      outputStream.write(
-        liveDataBufferProvider.get(
+      Files.newOutputStream(outputPath, StandardOpenOption.CREATE).use { outputStream ->
+        val array = ByteArray(144) // 144 is FileHeader's length
+        headerBuffer.get(array)
+        outputStream.write(array)
+
+        val sessionInfoBytes = ByteArray(liveReader.fileHeader.sessionInfoLen)
+        val sessionInfoBuffer = liveDataBufferProvider.get(
           liveReader.fileHeader.sessionInfoOffset,
           liveReader.fileHeader.sessionInfoLen,
-        ).array()
-      )
+        )
+        sessionInfoBuffer.get(sessionInfoBytes)
+        outputStream.write(sessionInfoBytes)
 
-      for (buffer in buffers) {
-        outputStream.write(buffer)
+        val varHeaderBytes = ByteArray(liveReader.fileHeader.numVars * 144) // Each var header is 144 bytes
+        val varHeaderBuffer = liveDataBufferProvider.get(
+          liveReader.fileHeader.varHeaderOffset,
+          varHeaderBytes.size,
+        )
+        varHeaderBuffer.get(varHeaderBytes)
+        outputStream.write(varHeaderBytes)
+
+        for (buffer in buffers) {
+          outputStream.write(buffer)
+        }
       }
+    } catch (e: Exception) {
+      e.printStackTrace()
     }
   }
 
   fun stop() {
-    isRunning.set(true)
+    isRunning.set(false)
   }
 }
 
 fun main() {
-  val outputPath = Paths.get("livedata.ibt")
+  val currentUser = System.getProperty("user.name")
+  val outputPath = Paths.get("C:\\Users\\$currentUser\\Downloads\\livedata.ibt")
   val logger = IRacingLiveDataLogger(outputPath)
-  logger.collectAndLog()
+  val thread = thread {
+    logger.collectAndLog()
+  }
 
   println("Reading from live data...")
   val scanner = Scanner(System.`in`)
   scanner.nextLine()
   logger.stop()
 
-  println("Wrote to livedata.ibt")
+  println("Wrote to ${outputPath.absolute()}")
+
+  thread.join()
 }
